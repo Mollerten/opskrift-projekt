3e3ffdfccc07cc6476ea4ded41cb11e8556a411f
Altant <28458922+Altant457@users.noreply.github.com>
2022-10-27 11:15:19 +0200

    Lavet en knap der henter en tilfældig opskrift

--- src/components/OpskriftList.jsx
+++ src/components/OpskriftList.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Opskrift from "./Opskrift.jsx";
 
 function OpskriftList({opskrifter}) {
 
+    const [opskrift, setOpskrift] = useState({});
+
     function getRandomInt() {
-        const min = 1;
-        const max = opskrifter.map().size;
-        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+        const max = opskrifter.length;
+        return Math.floor(Math.random() * max); // The maximum is exclusive and the minimum is inclusive
     }
 
+    function getOpskrift() {
+        const nyOpskrift = opskrifter[getRandomInt()]
+        setOpskrift(nyOpskrift)
+    }
 
     return (
         <div>
-            <Opskrift
+            <button onClick={getOpskrift}>Hent opskrift</button>
+            <div>
+                {<Opskrift
+                    id={opskrift.id}
+                    navn={opskrift.navn}
+                    pris={opskrift.pris}
+                    type={opskrift.type}
+                    hyperlink={opskrift.hyperlink}
+                />}
+            </div>
 
-
-            />
-            {opskrifter[getRandomInt()]}
-
-            {opskrifter.map (opskrift =>
-               <Opskrift key={opskrift.id}
-                   id={opskrift.id}
-                   navn={opskrift.navn}
-                   pris={opskrift.pris}
-                   type={opskrift.type}
-                   hyperlink={opskrift.hyperlink}
-               />
-            )}
-
+            {/*{opskrifter.map (opskrift =>*/}
+            {/*   <Opskrift key={opskrift.id}*/}
+            {/*       id={opskrift.id}*/}
+            {/*       navn={opskrift.navn}*/}
+            {/*       pris={opskrift.pris}*/}
+            {/*       type={opskrift.type}*/}
+            {/*       hyperlink={opskrift.hyperlink}*/}
+            {/*   />*/}
+            {/*)}*/}
         </div>
     );
 }
